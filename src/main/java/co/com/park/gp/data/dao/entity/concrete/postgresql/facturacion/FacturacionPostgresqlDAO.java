53b00dc779f582d41546bd17a6423803e6467aa9
David-alzate <alzatedavid0126@gmail.com>
2024-09-07 16:19:10 -0500

    Se termino la implementacion de los metodos DAO de consulta para la facturacion y el metodo de pago

--- src/main/java/co/com/park/gp/data/dao/entity/concrete/postgresql/facturacion/FacturacionPostgresqlDAO.java
+++ src/main/java/co/com/park/gp/data/dao/entity/concrete/postgresql/facturacion/FacturacionPostgresqlDAO.java
@@ -1,15 +1,22 @@
 package co.com.park.gp.data.dao.entity.concrete.postgresql.facturacion;
 
 import co.com.park.gp.crosscutting.exceptions.custom.DataGPException;
+import co.com.park.gp.crosscutting.helpers.ObjectHelper;
+import co.com.park.gp.crosscutting.helpers.UUIDHelper;
 import co.com.park.gp.crosscutting.messagecatalog.MessageCatalogStrategy;
 import co.com.park.gp.crosscutting.messagecatalog.data.CodigoMensaje;
 import co.com.park.gp.data.dao.entity.concrete.SqlConnection;
 import co.com.park.gp.data.dao.entity.facturacion.FacturacionDAO;
 import co.com.park.gp.entity.facturacion.FacturacionEntitiy;
+import co.com.park.gp.entity.facturacion.MetodoPagoEntity;
+import co.com.park.gp.entity.sesionesparqueo.SesionParqueoEntity;
+import co.com.park.gp.entity.tarifas.TarifaEntity;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -75,7 +82,98 @@ public class FacturacionPostgresqlDAO extends SqlConnection implements Facturaci
 
     @Override
     public List<FacturacionEntitiy> consultar(FacturacionEntitiy data) {
-        return List.of();
+        final StringBuilder sentenciaSql = new StringBuilder();
+        sentenciaSql.append("SELECT f.id, sp.id AS idSesionParqueo, sp.fechaHoraIngreso, sp.fechaHoraSalida, ");
+        sentenciaSql.append("t.id AS idTarifa, t.nombre AS tarifa, f.valorPagar, mp.id AS idMetodoPago, mp.nombre AS metodoPago ");
+        sentenciaSql.append("FROM facturacion f ");
+        sentenciaSql.append("INNER JOIN sesion_parqueo sp ON sp.id = f.sesion_parqueo_id ");
+        sentenciaSql.append("INNER JOIN tarifa t ON t.id = f.tarifa_id ");
+        sentenciaSql.append("INNER JOIN metodo_pago mp ON mp.id = f.metodo_pago_id ");
+        sentenciaSql.append("WHERE 1=1 ");
+
+        final List<Object> parametros = new ArrayList<>();
+
+        if (!ObjectHelper.getObjectHelper().isNull(data.getId()) && !data.getId().equals(UUIDHelper.getDefault())) {
+            sentenciaSql.append(" AND f.id = ?");
+            parametros.add(data.getId());
+        }
+
+        if (!ObjectHelper.getObjectHelper().isNull(data.getSesionParqueo())
+                && !ObjectHelper.getObjectHelper().isNull(data.getSesionParqueo().getId())
+                && !data.getSesionParqueo().getId().equals(UUIDHelper.getDefault())) {
+            sentenciaSql.append(" AND sp.id = ?");
+            parametros.add(data.getSesionParqueo().getId());
+        }
+
+        if (!ObjectHelper.getObjectHelper().isNull(data.getDuracion())) {
+            sentenciaSql.append(" AND sp.fechaHoraSalida - sp.fechaHoraIngreso = ?");
+            parametros.add(data.getDuracion().toMillis()); // Convertimos la duración a milisegundos
+        }
+
+        if (!ObjectHelper.getObjectHelper().isNull(data.getTarifa())
+                && !ObjectHelper.getObjectHelper().isNull(data.getTarifa().getId())
+                && !data.getTarifa().getId().equals(UUIDHelper.getDefault())) {
+            sentenciaSql.append(" AND t.id = ?");
+            parametros.add(data.getTarifa().getId());
+        }
+
+        if (data.getValorPagar() != null) {
+            sentenciaSql.append(" AND f.valorPagar = ?");
+            parametros.add(data.getValorPagar());
+        }
+
+        if (!ObjectHelper.getObjectHelper().isNull(data.getMetodoPago())
+                && !ObjectHelper.getObjectHelper().isNull(data.getMetodoPago().getId())
+                && !data.getMetodoPago().getId().equals(UUIDHelper.getDefault())) {
+            sentenciaSql.append(" AND mp.id = ?");
+            parametros.add(data.getMetodoPago().getId());
+        }
+
+        final List<FacturacionEntitiy> facturaciones = new ArrayList<>();
+
+        try (final PreparedStatement sentenciaSqlPreparada = getConexion().prepareStatement(sentenciaSql.toString())) {
+            for (int i = 0; i < parametros.size(); i++) {
+                sentenciaSqlPreparada.setObject(i + 1, parametros.get(i));
+            }
+
+            try (final ResultSet resultado = sentenciaSqlPreparada.executeQuery()) {
+                while (resultado.next()) {
+                    FacturacionEntitiy facturacion = new FacturacionEntitiy();
+                    facturacion.setId(UUIDHelper.convertToUUID(resultado.getString("id")));
+                    facturacion.setValorPagar(resultado.getBigDecimal("valorPagar"));
+
+                    SesionParqueoEntity sesionParqueo = SesionParqueoEntity.build();
+                    sesionParqueo.setId(UUIDHelper.convertToUUID(resultado.getString("idSesionParqueo")));
+                    sesionParqueo.setFechaHoraIngreso(resultado.getTimestamp("fechaHoraIngreso").toLocalDateTime());
+                    sesionParqueo.setFechaHoraSalida(resultado.getTimestamp("fechaHoraSalida").toLocalDateTime());
+                    facturacion.setSesionParqueo(sesionParqueo);
+
+                    TarifaEntity tarifa = TarifaEntity.build();
+                    tarifa.setId(UUIDHelper.convertToUUID(resultado.getString("idTarifa")));
+                    tarifa.setTarifa(resultado.getLong("tarifa"));
+                    facturacion.setTarifa(tarifa);
+
+                    MetodoPagoEntity metodoPago = MetodoPagoEntity.build();
+                    metodoPago.setId(UUIDHelper.convertToUUID(resultado.getString("idMetodoPago")));
+                    metodoPago.setNombre(resultado.getString("metodoPago"));
+                    facturacion.setMetodoPago(metodoPago);
+
+                    facturaciones.add(facturacion);
+                }
+            }
+
+        } catch (SQLException excepcion) {
+            var mensajeUsuario = "Se ha presentado un problema tratando de consultar la Facturación...";
+            var mensajeTecnico = "Se ha presentado una SQLException tratando de realizar el select de Facturación en la tabla \"Facturación\" de la base de datos.";
+            throw new DataGPException(mensajeUsuario, mensajeTecnico, excepcion);
+
+        } catch (Exception excepcion) {
+            var mensajeUsuario = "Se ha presentado un problema tratando de consultar la Facturación...";
+            var mensajeTecnico = "Se ha presentado una INESPERADO tratando de realizar el Select Facturación en la tabla \"Facturación\" de la base de datos.";
+            throw new DataGPException(mensajeUsuario, mensajeTecnico, excepcion);
+        }
+
+        return facturaciones;
     }
 
     @Override
--- src/main/java/co/com/park/gp/data/dao/entity/concrete/postgresql/facturacion/MetodoPagoPostgresqlDAO.java
+++ src/main/java/co/com/park/gp/data/dao/entity/concrete/postgresql/facturacion/MetodoPagoPostgresqlDAO.java
@@ -1,10 +1,18 @@
 package co.com.park.gp.data.dao.entity.concrete.postgresql.facturacion;
 
+import co.com.park.gp.crosscutting.exceptions.custom.DataGPException;
+import co.com.park.gp.crosscutting.helpers.ObjectHelper;
+import co.com.park.gp.crosscutting.helpers.TextHelper;
+import co.com.park.gp.crosscutting.helpers.UUIDHelper;
 import co.com.park.gp.data.dao.entity.concrete.SqlConnection;
 import co.com.park.gp.data.dao.entity.facturacion.MetodoPagoDAO;
 import co.com.park.gp.entity.facturacion.MetodoPagoEntity;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MetodoPagoPostgresqlDAO extends SqlConnection implements MetodoPagoDAO {
@@ -15,6 +23,51 @@ public class MetodoPagoPostgresqlDAO extends SqlConnection implements MetodoPago
 
     @Override
     public List<MetodoPagoEntity> consultar(MetodoPagoEntity data) {
-        return List.of();
+        final StringBuilder sentenciaSql = new StringBuilder();
+        sentenciaSql.append("SELECT mp.id, mp.nombre ");
+        sentenciaSql.append("FROM metodo_pago mp ");
+        sentenciaSql.append("WHERE 1=1 ");
+
+        final List<Object> parametros = new ArrayList<>();
+
+        if (!ObjectHelper.getObjectHelper().isNull(data.getId()) && !data.getId().equals(UUIDHelper.getDefault())) {
+            sentenciaSql.append(" AND mp.id = ?");
+            parametros.add(data.getId());
+        }
+
+        if (!TextHelper.isNullOrEmpty(data.getNombre())) {
+            sentenciaSql.append(" AND LOWER(mp.nombre) = ?");
+            parametros.add(TextHelper.convertToLowercase(data.getNombre()));
+        }
+
+        final List<MetodoPagoEntity> metodosPago = new ArrayList<>();
+
+        try (final PreparedStatement sentenciaSqlPreparada = getConexion().prepareStatement(sentenciaSql.toString())) {
+            for (int i = 0; i < parametros.size(); i++) {
+                sentenciaSqlPreparada.setObject(i + 1, parametros.get(i));
+            }
+
+            try (final ResultSet resultado = sentenciaSqlPreparada.executeQuery()) {
+                while (resultado.next()) {
+                    MetodoPagoEntity metodoPago = new MetodoPagoEntity();
+                    metodoPago.setId(UUIDHelper.convertToUUID(resultado.getString("id")));
+                    metodoPago.setNombre(resultado.getString("nombre"));
+                    metodosPago.add(metodoPago);
+                }
+            }
+
+        } catch (SQLException excepcion) {
+            var mensajeUsuario = "Se ha presentado un problema tratando de consultar el Método de Pago...";
+            var mensajeTecnico = "Se ha presentado una SQLException tratando de realizar el select de Método de Pago en la tabla \"Método de Pago\" de la base de datos.";
+            throw new DataGPException(mensajeUsuario, mensajeTecnico, excepcion);
+
+        } catch (Exception excepcion) {
+            var mensajeUsuario = "Se ha presentado un problema tratando de consultar el Método de Pago...";
+            var mensajeTecnico = "Se ha presentado un problema INESPERADO tratando de realizar el Select Método de Pago en la tabla \"Método de Pago\" de la base de datos.";
+            throw new DataGPException(mensajeUsuario, mensajeTecnico, excepcion);
+        }
+
+        return metodosPago;
     }
+
 }
