3fd5f9aec54e51bd88e3f3c3be861cd90aaecb57
David-alzate <alzatedavid0126@gmail.com>
2024-11-14 15:22:27 -0500

    Se arreglo la instancia de bloqueo con doble verificacion en el estadoEnum

--- src/main/java/co/com/park/gp/crosscutting/enums/EstadoEnum.java
+++ src/main/java/co/com/park/gp/crosscutting/enums/EstadoEnum.java
@@ -21,20 +21,19 @@ public enum EstadoEnum {
     }
 
     public UUID getId(DAOFactory factory) {
-        if (id == null) {
-            synchronized (this) {
-                if (id == null) {
-                    var estadoEntity = factory.getEstadoDAO().consultarPorDescripcion(this.getNombre());
-                    if (estadoEntity == null || estadoEntity.getId() == null) {
-                        throw new BusinessGPException(
-                            "Error al consultar el estado " + this.getNombre() + " en la base de datos",
-                            "El estado no existe o no tiene ID asociado"
-                        );
-                    }
-                    this.id = estadoEntity.getId();
+        synchronized (this) {
+            if (id == null) {
+                var estadoEntity = factory.getEstadoDAO().consultarPorDescripcion(this.getNombre());
+                if (estadoEntity == null || estadoEntity.getId() == null) {
+                    throw new BusinessGPException(
+                        "Error al consultar el estado " + this.getNombre() + " en la base de datos",
+                        "El estado no existe o no tiene ID asociado"
+                    );
                 }
+                this.id = estadoEntity.getId();
             }
         }
         return id;
     }
+
 }
